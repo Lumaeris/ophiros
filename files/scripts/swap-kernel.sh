877cfe2f2db8aaac66839c29d5b354b50dd022fe
Jill Fiore <contact@lumaeris.com>
2026-01-29 13:56:20 +0700

    chore: swap cachyos kernel to bazzite
    I'll swap to ogc kernel eventually when it will be ready

--- files/scripts/swap-kernel.sh
+++ files/scripts/swap-kernel.sh
@@ -9,12 +9,6 @@ dnf5 -y remove kernel* && rm -r -f /usr/lib/modules/*
 # exclude pulling kernel from fedora repos
 dnf5 -y config-manager setopt "*fedora*".exclude="kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-devel kernel-headers"
 
-# enable cachyos kernel copr repo
-dnf5 -y copr enable bieszczaders/kernel-cachyos
-
-# let selinux use cachyos kernel
-setsebool -P domain_kernel_load_modules on
-
 # create a shims to bypass kernel install triggering dracut/rpm-ostree
 # seems to be minimal impact, but allows progress on build
 pushd /usr/lib/kernel/install.d
@@ -25,18 +19,31 @@ printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
 chmod +x 05-rpmostree.install 50-dracut.install
 popd
 
-dnf5 -y install --allowerasing kernel-cachyos kernel-cachyos-devel-matched
+pkgs=(
+    kernel
+    kernel-core
+    kernel-modules
+    kernel-modules-core
+    kernel-modules-extra
+    kernel-modules-akmods
+    kernel-devel
+    kernel-devel-matched
+    kernel-tools
+    kernel-tools-libs
+    kernel-common
+)
+
+PKG_PAT=()
+for pkg in "${pkgs[@]}"; do
+    # FIXME: assumes the kernel starts with version 6
+    PKG_PAT+=("/tmp/kernel/${pkg}-6"*)
+done
+
+dnf5 -y install ${PKG_PAT[@]}
+
+dnf5 versionlock add $pkgs
 
 pushd /usr/lib/kernel/install.d
 mv -f 05-rpmostree.install.bak 05-rpmostree.install
 mv -f 50-dracut.install.bak 50-dracut.install
 popd
-
-# enable cachyos kernel addons copr repo
-dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
-
-# install scx-scheds
-dnf5 -y install scx-scheds
-
-dnf5 -y copr disable bieszczaders/kernel-cachyos
-dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
