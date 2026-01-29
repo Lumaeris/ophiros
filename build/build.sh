911940a75d6e77d7c1dfc440ee3b213bc0dc4307
Jill Fiore <contact@lumaeris.com>
2026-01-29 18:42:28 +0700

    chore: add steam, gamescope and shadowblip rpms

--- build/build.sh
+++ build/build.sh
@@ -22,12 +22,31 @@ dnf5 -y remove ublue-os-udev-rules \
     cosign \
     toolbox
 
+rm -f /etc/yum.repos.d/_copr_ublue-os-akmods.repo
+
 cp -avf "/ctx/files"/. /
 
 /ctx/swap-kernel.sh
 
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
+dnf5 -y install --setopt=install_weak_deps=False steam
 
+dnf5 -y copr enable ublue-os/bazzite
+dnf5 -y copr enable ublue-os/bazzite-multilib
+dnf5 -y config-manager setopt '*bazzite*'.priority=90
+dnf5 -y install gamescope-libs gamescope-shaders
+dnf5 -y copr disable ublue-os/bazzite
+dnf5 -y copr disable ublue-os/bazzite-multilib
+
+OGUI_TAG=$(/ctx/ghcurl https://api.github.com/repos/ShadowBlip/OpenGamepadUI/releases/latest | grep tag_name | cut -d : -f2 | tr -d "v\", ")
+IP_TAG=$(/ctx/ghcurl https://api.github.com/repos/ShadowBlip/InputPlumber/releases/latest | grep tag_name | cut -d : -f2 | tr -d "v\", ")
+PS_TAG=$(/ctx/ghcurl https://api.github.com/repos/ShadowBlip/PowerStation/releases/latest | grep tag_name | cut -d : -f2 | tr -d "v\", ")
+
+dnf5 -y install https://github.com/ShadowBlip/OpenGamepadUI/releases/download/v${OGUI_TAG}/opengamepadui-${OGUI_TAG}-1.x86_64.rpm \
+    https://github.com/ShadowBlip/InputPlumber/releases/download/v${IP_TAG}/inputplumber-${IP_TAG}-1.x86_64.rpm \
+    https://github.com/ShadowBlip/PowerStation/releases/download/v${PS_TAG}/powerstation-${PS_TAG}-1.x86_64.rpm
+
+sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/htop.desktop
+sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/nvtop.desktop
 rm -rf /usr/src
 rm -rf /usr/share/doc
 rm -rf /usr/share/man
