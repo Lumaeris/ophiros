9df101ae981026e3afe7a998dfb4eba1980f0c62
Jill Fiore <contact@lumaeris.com>
2026-02-06 20:40:05 +0700

    chore: download steam to /tmp

--- build/build.sh
+++ build/build.sh
@@ -35,10 +35,9 @@ cp -avf "/ctx/files"/. /
 dnf5 -y install --setopt=install_weak_deps=False steam sddm
 
 # get steam and extract big bootstrap file from it
+curl --retry 3 -Lo /tmp/steam.tar.zst https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/steam-jupiter-stable-1.0.0.85-2-x86_64.pkg.tar.zst
 mkdir -p /usr/share/gamescope-session-plus/
-curl --retry 3 -Lo /usr/share/gamescope-session-plus/steam.tar.zst https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/steam-jupiter-stable-1.0.0.85-2-x86_64.pkg.tar.zst
-tar -I zstd -xvf "/usr/share/gamescope-session-plus/steam.tar.zst" usr/lib/steam/bootstraplinux_ubuntu12_32.tar.xz -O > "/usr/share/gamescope-session-plus/steam_boostrap.tar.xz"
-rm -f /usr/share/gamescope-session-plus/steam.tar.zst
+tar -I zstd -xvf "/tmp/steam.tar.zst" usr/lib/steam/bootstraplinux_ubuntu12_32.tar.xz -O > "/usr/share/gamescope-session-plus/steam_boostrap.tar.xz"
 setfattr -n user.components -v "steam-bootstrap" /usr/share/gamescope-session-plus/steam_boostrap.tar.xz
 
 # enable bunch of repos, install gamescope{,-session} and steam deck dsp (for oled model)
