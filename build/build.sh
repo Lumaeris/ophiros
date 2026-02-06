871e8a373f1603254b1af5bbf42c41ed0feaa2e8
Jill Fiore <contact@lumaeris.com>
2026-02-06 20:31:38 +0700

    feat: install gamescope session and get proper steam bootstrap

--- build/build.sh
+++ build/build.sh
@@ -2,8 +2,8 @@
 
 set -xeuo pipefail
 
+# remove leftovers
 dnf5 config-manager setopt fedora-cisco-openh264.enabled=0
-
 dnf5 -y remove ublue-os-udev-rules \
     ublue-os-update-services \
     ublue-os-signing \
@@ -23,40 +23,53 @@ dnf5 -y remove ublue-os-udev-rules \
     toolbox \
     gnome-tour \
     gnome-classic-session
-
 rm -f /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 
+# copy system files over
 cp -avf "/ctx/files"/. /
 
+# swap to bazzite kernel
 /ctx/swap-kernel.sh
 
-dnf5 -y install --setopt=install_weak_deps=False steam
+# install steam and sddm (the latter is needed for gamescope session)
+dnf5 -y install --setopt=install_weak_deps=False steam sddm
 
+# get steam and extract big bootstrap file from it
+mkdir -p /usr/share/gamescope-session-plus/
+curl --retry 3 -Lo /usr/share/gamescope-session-plus/steam.tar.zst https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/steam-jupiter-stable-1.0.0.85-2-x86_64.pkg.tar.zst
+tar -I zstd -xvf "/usr/share/gamescope-session-plus/steam.tar.zst" usr/lib/steam/bootstraplinux_ubuntu12_32.tar.xz -O > "/usr/share/gamescope-session-plus/steam_boostrap.tar.xz"
+rm -f /usr/share/gamescope-session-plus/steam.tar.zst
+setfattr -n user.components -v "steam-bootstrap" /usr/share/gamescope-session-plus/steam_boostrap.tar.xz
+
+# enable bunch of repos, install gamescope{,-session} and steam deck dsp (for oled model)
 dnf5 -y copr enable ublue-os/bazzite
 dnf5 -y copr enable ublue-os/bazzite-multilib
 dnf5 -y copr enable ycollet/audinux
 dnf5 -y config-manager setopt '*bazzite*'.priority=90
-dnf5 -y install gamescope-libs gamescope-shaders steamdeck-dsp
+dnf5 -y install gamescope-libs gamescope-shaders steamdeck-dsp gamescope-session-plus
 dnf5 -y copr disable ublue-os/bazzite
 dnf5 -y copr disable ublue-os/bazzite-multilib
 dnf5 -y copr disable ycollet/audinux
 
+# get latest versions of shadowblip software from github and install it directly
 OGUI_TAG=$(/ctx/ghcurl https://api.github.com/repos/ShadowBlip/OpenGamepadUI/releases/latest | grep tag_name | cut -d : -f2 | tr -d "v\", ")
 IP_TAG=$(/ctx/ghcurl https://api.github.com/repos/ShadowBlip/InputPlumber/releases/latest | grep tag_name | cut -d : -f2 | tr -d "v\", ")
 PS_TAG=$(/ctx/ghcurl https://api.github.com/repos/ShadowBlip/PowerStation/releases/latest | grep tag_name | cut -d : -f2 | tr -d "v\", ")
-
 dnf5 -y install https://github.com/ShadowBlip/OpenGamepadUI/releases/download/v${OGUI_TAG}/opengamepadui-${OGUI_TAG}-1.x86_64.rpm \
     https://github.com/ShadowBlip/InputPlumber/releases/download/v${IP_TAG}/inputplumber-${IP_TAG}-1.x86_64.rpm \
     https://github.com/ShadowBlip/PowerStation/releases/download/v${PS_TAG}/powerstation-${PS_TAG}-1.x86_64.rpm
 
+# install homebrew from secureblue (mostly the same as ublue's brew oci)
 dnf5 -y copr enable secureblue/homebrew
 dnf5 -y install homebrew
 dnf5 -y copr disable secureblue/homebrew
 
+# install bazaar
 dnf5 -y copr enable ublue-os/packages
 dnf5 -y install bazaar
 dnf5 -y copr disable ublue-os/packages
 
+# finishing touches
 glib-compile-schemas /usr/share/glib-2.0/schemas
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/htop.desktop
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/nvtop.desktop
@@ -64,13 +77,12 @@ rm -rf /usr/src
 rm -rf /usr/share/doc
 rm -rf /usr/share/man
 rpm --erase --nodeps kernel-devel
-
 flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 systemctl mask flatpak-add-fedora-repos.service
-
 #sed -i 's|uupd|& --disable-module-distrobox|' /usr/lib/systemd/system/uupd.service
 dnf5 clean all
 
+# regenerate initramfs (since we no longer have it)
 KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
 export DRACUT_NO_XATTR=1
 dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
